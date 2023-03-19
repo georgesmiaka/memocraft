c5391700b17cb6ed3c5a8608392128b7f1d01a00
gmiak <kayss.g@gmail.com>
2023-03-19 23:18:23 +0100

    Added welcome message on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to memocraft</p>
+        <img src='../assets/logo2.png' className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The virtual version of G!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/people/Georges-Kayembe-Miaka-Pro/100089995069982/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>
